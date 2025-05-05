d8c2838cbd7de4d434a3b93c3e86edbc39564006
mohamed ayad <mo.ayad.9@outlook.com>
2025-05-05 17:58:34 +0300

    I have linked the services with the component

--- src/app/component/add-employee/add-employee.component.ts
+++ src/app/component/add-employee/add-employee.component.ts
@@ -1,32 +1,69 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import {MatSelectModule} from '@angular/material/select';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {provideNativeDateAdapter} from '@angular/material/core';
-import {MatIconModule} from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { Employee } from '../../model/Employee';
+import { MatDialogRef } from '@angular/material/dialog';
+import { EmployeeService } from '../../service/employee.service';
 
 @Component({
   selector: 'app-add-employee',
-  imports: [MatIconModule,MatCardModule, ReactiveFormsModule,MatFormFieldModule, MatInputModule, MatButtonModule, MatSelectModule, MatDatepickerModule],
+  imports: [
+    MatIconModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatDatepickerModule,
+  ],
   providers: [provideNativeDateAdapter()],
   templateUrl: './add-employee.component.html',
-  styleUrl: './add-employee.component.css'
+  styleUrl: './add-employee.component.css',
 })
 export class AddEmployeeComponent {
-  title = 'Add Employee';
-  empForm=new FormGroup({
-    id:new FormControl(0),
-    name:new FormControl('',Validators.required),
-    doj:new FormControl(new Date(),Validators.required),
-    role:new FormControl('',Validators.required),
-    salary:new FormControl(0,Validators.required),
-  })
+  constructor(
+    private service: EmployeeService,
+    private ref: MatDialogRef<AddEmployeeComponent>
+  ) {}
 
-  saveEmployee(){
-    console.log(this.empForm.value);
+  title = 'Add Employee';
+  empForm = new FormGroup({
+    id: new FormControl(0),
+    name: new FormControl('', Validators.required),
+    doj: new FormControl(new Date(), Validators.required),
+    role: new FormControl('', Validators.required),
+    salary: new FormControl(0, Validators.required),
+  });
+
+  SaveEmployee() {
+    if (this.empForm.valid) {
+      let _data: Employee = {
+        id: this.empForm.value.id as number,
+        name: this.empForm.value.name as string,
+        doj: new Date(this.empForm.value.doj as Date),
+        role: this.empForm.value.role as string,
+        salary: this.empForm.value.salary as number,
+      };
+      this.service.Create(_data).subscribe((item) => {
+        alert(`Employee Created`);
+        this.closepopup();
+      });
+    }
+  }
+  closepopup() {
+    this.ref.close();
   }
 }
--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Employee } from '../model/Employee';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmployeeService {
+  apiUrl = 'http://localhost:3000/employees';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  GetAll() {
+    return this.http.get<Employee[]>(this.apiUrl);
+  }
+  Get(empId: number) {
+    return this.http.get<Employee>(this.apiUrl + '/' + empId);
+  }
+  Create(data: Employee) {
+    return this.http.post(this.apiUrl, data);
+  }
+  Update(data: Employee) {
+    return this.http.put(this.apiUrl + '/' + data.id, data);
+  }
+  Delete(empId: Employee) {
+    return this.http.delete(this.apiUrl + '/' + empId);
+  }
 }
